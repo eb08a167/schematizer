c90558505834e68d987495753d330e5bb97059bd
Andrew Kiyko <eb08a167@gmail.com>
2017-09-29 20:09:14 +0300

    :sparkles: Add RangeValidator

--- schematizer/validators.py
+++ schematizer/validators.py
@@ -41,5 +41,25 @@ class LengthValidator(BaseValidator):
             })
 
 
+class RangeValidator(BaseValidator):
+    def __init__(self, min=None, max=None):
+        self.min = min
+        self.max = max
+
+    def validate_native(self, obj):
+        if self.min is not None and obj < self.min:
+            code = 'TOO_SMALL'
+        elif self.max is not None and obj > self.max:
+            code = 'TOO_BIG'
+        else:
+            code = None
+        if code:
+            raise SimpleValidationError(code, extra={
+                'min': self.min,
+                'max': self.max,
+            })
+
+
 Nullable = NullableValidator
 Length = LengthValidator
+Range = RangeValidator
