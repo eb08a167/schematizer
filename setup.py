be67dcef0d29c4be767b9a6d94ad0616313f7aef
Andrew Kiyko <eb08a167@gmail.com>
2017-10-22 14:24:55 +0300

    :up: Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as fp:
 
 setup(
     name='schematizer',
-    version='0.6.1',
+    version='0.7.0',
     description='A lightweight library for data marshalling/unmarshalling in Python',
     long_description=long_description,
     url='https://github.com/eb08a167/schematizer',
